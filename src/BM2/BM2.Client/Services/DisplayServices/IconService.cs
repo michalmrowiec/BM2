aa4af4f413c59a55914ff7a05cad4182d8f053a4
Michał Mrowiec <michalmro210101@gmail.com>
2025-02-27 20:41:19 +0100

    Add icon to chip in status column in records list

---
+++ src/BM2/BM2.Client/Services/DisplayServices/IconService.cs
@@ -0,0 +1,21 @@
+﻿using BM2.Shared.DTOs;
+using BM2.Shared.SystemCodes;
+using MudBlazor;
+
+namespace BM2.Client.Services.DisplayServices;
+
+public static class IconService
+{
+    public static string GetIconForRecordStatus(this RecordStatusDTO recordStatus)
+    {
+        return recordStatus.SystemCode switch
+        {
+            var code when code == StatusSystemCode.Planned => Icons.Material.Filled.PlaylistAddCheckCircle,
+            var code when code == StatusSystemCode.Realized => Icons.Material.Filled.CheckCircle,
+            var code when code == StatusSystemCode.Active => Icons.Material.Filled.PlayCircle,
+            var code when code == StatusSystemCode.Suspended => Icons.Material.Filled.StopCircle,
+            var code when code == StatusSystemCode.Pending => Icons.Material.Filled.AccessTimeFilled,
+            _ => Icons.Material.Filled.CheckCircle
+        };
+    }
+}
