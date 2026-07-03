bf4058e7eb395cd173e8e244427790ade0a4d437
Michał Mrowiec <michalmro210101@gmail.com>
2026-07-04 00:23:33 +0200

    Add status and tags cols and filters for record grid

--- src/BM2.Shared/TransactionFilter.cs
+++ src/BM2.Shared/TransactionFilter.cs
@@ -2,12 +2,29 @@
 
 public class TransactionFilter
 {
+    public enum LogicalOperator
+    {
+        Or = 0,
+        And = 1
+    }
+
     public string? SearchText { get; set; }
     public DateTime? DateFrom { get; set; }
     public DateTime? DateTo { get; set; }
 
     // Multi-select filtry (puste listy oznaczają "brak filtra / wybierz wszystkie")
-    public List<Guid> AccountIds { get; set; } = new();
-    public List<Guid> CategoryIds { get; set; } = new();
-    public List<Guid> TagIds { get; set; } = new();
+    public List<Guid> AccountIds { get; set; } = [];
+    public List<Guid> StatusIds { get; set; } = [];
+    public List<Guid> CategoryIds { get; set; } = [];
+    public List<Guid> TagIds { get; set; } = [];
+    public LogicalOperator TagOperator { get; set; } = LogicalOperator.Or;
+
+    public bool IsEmpty() =>
+        //string.IsNullOrEmpty(SearchText) &&
+        !DateFrom.HasValue
+        && !DateTo.HasValue
+        && !AccountIds.Any()
+        && !StatusIds.Any()
+        && !CategoryIds.Any()
+        && !TagIds.Any();
 }
